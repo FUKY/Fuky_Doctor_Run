56d26e89a5f76ff74d225fb19ff82f7d16f164c5
toneylee <ducsangpro.93@gmail.com>
2015-05-10 16:52:14 +0700

    Rap giao dien

---
+++ Technical/Fuky_Doctor_Run/Assets/GUIMedicineController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIMedicineController : MonoBehaviour {
+
+    private int MedicineCount;
+    public Image[] medecineSlot;
+
+    void Start()
+    {
+        ResourceController.Instance.InitData();
+        MedicineCount = ResourceController.Instance.MedicineLibrary.Count;
+    }
+
+    [ContextMenu("Test")]
+    public void RandomSlot()
+    {
+        if (MedicineCount == 0)
+        {
+#if UNITY_EDITOR    
+            Debug.LogError("Khong load duoc du lieu");
+#endif
+            return;
+        }
+        int index = Random.Range(1, MedicineCount);
+        foreach (Image item in medecineSlot)
+        {
+            item.sprite = ResourceController.Instance.MedicineLibrary[index];
+            index = Random.Range(0, MedicineCount);
+        }
+    }
+}
---
+++ Technical/Fuky_Doctor_Run/Assets/ResourceController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ResourceController : Singleton<ResourceController>
+{
+    private string MEDICINE_PATH = "Enviroment/Game/Medicine";
+    public Dictionary<int, Sprite> MedicineLibrary;
+
+    void Start()
+    {
+        //MedicineLibrary = new Dictionary<int, Sprite>();
+    }
+
+    public void InitData()
+    {
+        MedicineLibrary = new Dictionary<int, Sprite>();
+        LoadData(ref MedicineLibrary, MEDICINE_PATH);
+    }
+
+    public void LoadData(ref Dictionary<int, Sprite> dicSprites, string path)
+    {
+        Sprite[] sprites = Resources.LoadAll<Sprite>(path);
+        int index = 0;
+        foreach(Sprite sprite in sprites)
+        {
+            MedicineLibrary.Add(index, sprite);
+            index++;
+        }
+    }
+}
